c9fc0e1d46b21995d9eaa4596f2e5d385430a142
Muhammad Noman <143292643+Muhammadnoman004@users.noreply.github.com>
2023-12-25 23:58:10 +0500

    Working

--- AdminRegister/AdminHome.js
+++ AdminRegister/AdminHome.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-storage.js";
 const firebaseConfig = {
     apiKey: "AIzaSyBCk9IrnMaQwaBIc4cE7icTWgyCRnd7nDI",
@@ -15,7 +15,7 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 
-let Upload = document.querySelector("#upload");
+
 let SaveBtn = document.querySelector("#Save");
 let productDiv = document.querySelector("#productDiv");
 let ImageOutput = document.querySelector("#imageOutput");
@@ -23,38 +23,89 @@ let imageoutDiv = document.querySelector(".imageoutDiv");
 let ItemImage = document.querySelector("#itemImg");
 let imageURL;
 
-Upload.addEventListener("click", async () => {
+// const mountainsRef = ref(storage, `Images/${ItemImage.files[0].name}`);
+// const uploadTask = uploadBytesResumable(mountainsRef, ItemImage.files[0]);
 
-    const mountainsRef = ref(storage, `Images/${ItemImage.files[0].name}`);
-    const uploadTask = uploadBytesResumable(mountainsRef, ItemImage.files[0]);
 
-    uploadTask.on('state_changed',
-        (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-                case 'paused':
-                    console.log('Upload is paused');
-                    break;
-                case 'running':
-                    console.log('Upload is running');
-                    break;
+const downloadImageUrl = (file) => {
+    return new Promise((resolve, reject) => {
+        const restaurantImageRef = ref(storage, `images/${file.name}`
+        );
+        const uploadTask = uploadBytesResumable(restaurantImageRef, file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case "paused":
+                        console.log('Upload is paused');
+                        break;
+                    case "running":
+                        console.log("running");
+                        break;
+                }
+            },
+            (error) => {
+                reject(error);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL) => {
+                        resolve(downloadURL);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
             }
-        },
-        (error) => {
-            console.log(error);
-        },
-        () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                imageURL = downloadURL
-                console.log('File available at', downloadURL);
-                imageoutDiv.style.display = "block"
-                ImageOutput.src = imageURL;
-            });
+        );
+    });
+};
+
+
+ItemImage.addEventListener("change", async () => {
+    if (ItemImage.files.length > 0) {
+        const file = ItemImage.files[0];
+        imageURL = await downloadImageUrl(file);
+        imageoutDiv.style.display = "block";
+        if (imageURL) {
+            ImageOutput.src = imageURL;
         }
-    )
-    
+    }
 })
+
+
+    //  get Data From FireBase  //
+
+const getData = async () => {
+
+    const querySnapshot = await getDocs(collection(db, "Add Product"));
+    querySnapshot.forEach((data) => {
+        console.log(data.data());
+
+        productDiv.style.display = "block"
+        productDiv.innerHTML += `<div><div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${data.data().Item_ImageURL}" id="proImg" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${data.data().Item_Name}</h5>
+              <h6 class="card-title">$ ${data.data().Item_Price}</h6>
+              <p class="card-text">${data.data().Item_Description}</p>
+              <p class="card-text"><small class="text-body-secondary">Last updated 2 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+        </div>`
+
+    });
+}
+getData();
 
 SaveBtn.addEventListener("click", async () => {
     let ItemName = document.querySelector("#Itemname");
@@ -79,39 +130,22 @@ SaveBtn.addEventListener("click", async () => {
                 Item_Category: ItemCate.value,
                 Item_Description: ItemDesc.value,
                 Item_Price: Price.value,
-                // Item_ImageURL : imageURL,
-
+                Item_ImageURL: imageURL,
+                Time: new Date().toLocaleString(),
             });
             console.log("Document written with ID: ", docRef.id);
         } catch (e) {
             console.error("Error adding document: ", e);
         }
         // window.location = "./AdminLogIn.html";
-        productDiv.style.display = "block"
-        productDiv.innerHTML = `<div><div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="${imageURL}" id="proImg" class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${ItemName.value}</h5>
-              <h6 class="card-title">$ ${Price.value}</h6>
-              <p class="card-text">${ItemDesc.value}</p>
-              <p class="card-text"><small class="text-body-secondary">Last updated 2 mins ago</small></p>
-            </div>
-          </div>
-        </div>
-      </div>
-        </div>`
 
     }
+    ItemName.value = ''
+    ItemCate.value = ''
+    ItemDesc.value = ''
+    Price.value = ''
+    ItemImage.value = ''
+    imageoutDiv.style.display = 'none'
+
+
 })
-
-
-// productDiv.innerHTML = `<div><h2>${ItemCate.value}</h2>
-// <div><img src="${imageURL}" alt=""></div>
-// <div><h3>${ItemName.value}</h3></div>
-// <div><h4>${Price.value}</h4></div>
-// <div><p>${ItemDesc.value}</p></div>
-// </div>`
